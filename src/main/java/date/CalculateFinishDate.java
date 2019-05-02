bddb35e21d703869b859b2a2a8f293dc7132b223
Castle <10086@qq.com>
2019-05-02 14:03:18 +0800

    通过开始日期以及耗费工作日天数，得到完成日期

--- src/main/java/date/CalculateFinishDate.java
+++ src/main/java/date/CalculateFinishDate.java
@@ -1,6 +1,11 @@
 package date;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,12 +14,19 @@ import org.junit.Test;
 
 import util.HttpUtil;
 
+/**
+ * 
+ * @author Castle
+ *
+ */
 public class CalculateFinishDate {
     @Test
-    public void calcfinishDate() {
-        
+    public void calcfinishDate() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        // 2019-05-27
+        System.out.println(getEndWorkDate(sdf.parse("2019-05-20"), 6));
     }
-    
+
     /**
      * 通过开始日期以及耗费工作日天数，得到完成日期
      * <p>
@@ -25,39 +37,34 @@ public class CalculateFinishDate {
      * <p>
      * 其中：工作日对应结果为 0, 休息日对应结果为 1, 节假日对应的结果为 2
      * 
-     * @param startDate
-     *            起点日期
-     * @param period
-     *            预计所需工作日天数
+     * @param startDate 起点日期
+     * @param period    预计所需工作日天数
      * @return 预计完成日期
      * 
-     * @since 1.7
+     * @since 1.8
      * 
      * @author 黄嘉明
      * @version 2017-05-26
      */
     public static LocalDate getEndWorkDate(Date startDate, int period) {
-        if (null == startDate) {
-            startDate = new Date();
-        }
-        if (1 > period) {
-            System.out.println("所需工作日至少是1天，即当天完成");
-            return null;
-        }
+        assertNotNull("开始日期不得为空", startDate);
+        assertNotNull("预计办理天数不得为空", period);
+        LocalDateTime ldt = LocalDateTime.ofInstant(startDate.toInstant(), ZoneId.systemDefault());
+        LocalDate start = ldt.toLocalDate();
         if (365 < period) {
             System.out.println("业务办理期限过长");
             return null;
         }
-        if (1 == period) {
-            return new LocalDate(startDate);
+        if (period <= 1) {
+            // 当天完成
+            return start;
         }
-        
+
         // 外部接口
         String dateApi = "http://www.easybots.cn/api/holiday.php";
         // 开始日期
-        LocalDate start = LocalDate.fromDateFields(startDate);
         boolean loop = true;
-        // 大致估算所需天数
+        // 大致估算所需天数（通常每5个工作日就有2天周末，每次调用外部接口时查询的日期为：预计工作日加上N个周末）
         int estimateDays = period + (period / 5) * 2;
         // 精确的完成日期
         String target = "";
@@ -73,7 +80,7 @@ public class CalculateFinishDate {
             paramMap.put("d", sb.toString());
             String result = HttpUtil.sendPost(dateApi, paramMap);
             result = result.substring(1, result.length() - 1);
-            // System.out.println("test output==============:" + result);
+             System.out.println("test output==============:" + result);
             String[] resultSplit = result.split(",");
             // 调用外部接口返回结果中的工作日天数
             int workDayCount = 0;
